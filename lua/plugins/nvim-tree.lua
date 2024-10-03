1460639061ea64a40b8da0b7ae7be46873a6d555
Anton Louis Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2024-10-03 16:35:39 -0300

    Its looking sexy

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -2,9 +2,10 @@ return {
 	"nvim-tree/nvim-tree.lua",
 	requires = { "nvim-tree/nvim-web-devicons" },
 	config = function()
-		view = {
-				side = "left",
-				width = 30,
+		require'nvim-tree'.setup {
+			view = {
+				side = 'right',
+			}
 		}
 	end
 }
