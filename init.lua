1a82451ae79953d507ec491fbfd156b23c4f91b5
Anton Louis Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2024-10-04 08:42:18 -0300

    dart auto formatting

--- init.lua
+++ init.lua
@@ -4,9 +4,11 @@ vim.o.shiftwidth = 2
 vim.o.expandtab = false
 vim.o.autoindent = true
 vim.o.smartindent = true
+vim.o.autoread = true
 
 -- Load lazy.nvim
 require("configs.lazy")
 
 -- Load keymaps
 require("configs.keymaps")
+
