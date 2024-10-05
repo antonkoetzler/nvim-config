f361518543f3f167cafcf7abeb8a94e812ecd572
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2024-10-04 21:25:13 -0300

    Edit config

--- install.sh
+++ install.sh
@@ -3,20 +3,15 @@
 # If you are on windows, run this script in git bash.
 
 git clone git@github.com:antonkoetzler/nvim-config
-rm nvim-config/install.sh nvim-config/README.md
 
 if [[ "$OSTYPE" == "linux-gnu"* || "$OSTYPE" == "darwin"* ]]; then
 	rm -rf ~/config/.nvim
-	mkdir ~/config/.nvim
-	mv nvim-config/* ~/.config/nvim
+	mv nvim-config/ ~/.config/nvim
 	echo "nvim configuration installed in ~/.config/nvim"
 elif [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" ]]; then
-echo "here"
-
-	rm -rf "$USERPROFILE/AppData/Local/nvim-data"
-	mkdir "$USERPROFILE/AppData/Local/nvim-data"
-	# mv nvim-config/* "$USERPROFILE/AppData/Local/nvim-data"
-	# echo "nvim configuration installed in $USERPROFILE/AppData/Local/nvim"
+	rm -rf $USERPROFILE/AppData/Local/nvim-data
+	mv nvim-config $USERPROFILE/AppData/Local/nvim-data
+	echo "nvim configuration installed in $USERPROFILE\AppData\Local\nvim"
 else
 	echo "Unknown operating system: $OSTYPE"
 fi
