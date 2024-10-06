5d1c27f99d77c8eb3b5da1a0cd3fd2daba8afc00
Anton Louis Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2024-10-06 03:15:20 -0300

    only show git bash on windows

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,6 +1,8 @@
 return {
 	'akinsho/toggleterm.nvim',
 	config = function()
+		local is_windows = vim.loop.os_uname().sysname == "Windows_NT"
+
 		require('toggleterm').setup{
 			size = 20,
 			open_mapping = [[<c-t>]],
@@ -9,7 +11,7 @@ return {
 			start_in_insert = true,
 			insert_mappings = true,
 			terminal_mappings = true,
-			shell = '"C:\\Program Files\\Git\\bin\\bash.exe"',
+			shell = is_windows and '"C:\\Program Files\\Git\\bin\\bash.exe"' or nil,
 		}
 	end,
 }
