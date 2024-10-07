e330d10437f8165af643f00f2c4fce097e131d7a
Anton Louis Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2024-10-07 08:01:13 -0300

    ijqweoijqweij

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -4,14 +4,15 @@ return {
 		local is_windows = vim.loop.os_uname().sysname == "Windows_NT"
 
 		require('toggleterm').setup{
-			size = 20,
-			open_mapping = [[<c-t>]],
-			shade_filetypes = {},
-			shade_terminals = true,
-			start_in_insert = true,
-			insert_mappings = true,
-			terminal_mappings = true,
-			shell = is_windows and '"C:\\Program Files\\Git\\bin\\bash.exe"' or nil,
+      size = 12,
+      open_mapping = [[<c-t>]], -- To open more than one terminal, use 2<c-t> or 3<c-t>.
+      shade_filetypes = {},
+      shade_terminals = true,
+      shading_factor = 1,
+      start_in_insert = true,
+      persist_size = true,
+      close_on_exit = true,
+      direction = "horizontal",
 		}
 	end,
 }
