86bab4396c1b5c2a8aa507fd8333938fa9bcf739
Anton Louis Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2024-10-15 19:52:44 -0300

    Fix split turning off lsp

--- lua/plugins/flutter-tools.lua
+++ lua/plugins/flutter-tools.lua
@@ -17,11 +17,26 @@ return {
           vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
           vim.api.nvim_buf_set_keymap(bufnr, 'n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
           vim.api.nvim_buf_set_keymap(bufnr, 'n', 'ge', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+          vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
 
           vim.api.nvim_create_autocmd('BufWritePre', {
             buffer = bufnr,
             callback = function()
               vim.lsp.buf.format({ bufnr = bufnr })
+            end,
+          })
+
+          -- Keep LSP active in unfocused buffers.
+          vim.api.nvim_create_autocmd('BufLeave', {
+            pattern = '*.dart',
+            callback = function()
+              local bufnr = vim.fn.bufnr()
+              local clients = vim.lsp.get_active_clients()
+              for _, client in pairs(clients) do
+                if client.attached_buffers[bufnr] then
+                  vim.lsp.buf_attach_client(bufnr, client.id)
+                end
+              end
             end,
           })
         end,
