1635c2f9717a1cf51a61ac11771bc5dd4f9ab2af
Anton Louis Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2024-11-10 03:29:16 -0300

    Spring cleaning

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,10 +17,8 @@ vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
   require('plugins.telescope'),       -- Good for file navegation (i.e. fuzzy searching, livegrep, etc).
-  require('plugins.lualine'),         -- Status bar with various information at the bottom of the screen.
   require('plugins.vim-fugitive'),    -- Lots of git features; I mainly use to solve merge conflicts.
   require('plugins.scope'),           -- Separate buffers shown in telescope by tab.
-  require('plugins.dashboard'),       -- Shows a kitty on startup.
   require('plugins.kommentary'),      -- Allows gc/gcc comments (for commenting lines of code) to work cross-platform.
   require('plugins.nvim-lspconfig'),  -- LSP: Base configuraiton plugin.
   require('plugins.mason'),           -- LSP: Language server installer.
@@ -29,7 +27,6 @@ require('lazy').setup({
   require('plugins.flutter-tools'),   -- LSP & other goodies for Flutter.
   require('plugins.git-blame'),       -- Git blame on lines.
   require('plugins.bufferline'),      -- Buffer tab bar.
-  require('plugins.diffview'),        -- Viewing git differences and also merge conflict resolver tool.
   require('plugins.neotree'),         -- File tree/explorer plugin.
 
   -- Color schemes.
