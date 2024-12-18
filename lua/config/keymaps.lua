c7f17f2336580fe9d99208557b1569e91a22cdc5
Anton Louis Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2024-12-18 03:32:06 -0300

    Back to being a vim lover

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,20 +2,22 @@
 
 local opts = { noremap = true, silent = true }
 
+
 -- Leader keys.
 vim.g.mapleader = ' '
 vim.g.maplocalleader = '\\'
 
 -- General shortcuts.
-vim.api.nvim_set_keymap('i', '<C-Backspace>', '<C-w>', opts)          -- Ctrl + backspace.
-vim.api.nvim_set_keymap('i', '<C-h>', '<C-w>', opts)                  -- Ctrl + backspace.
-vim.api.nvim_set_keymap('n', '<leader>tc', '<cmd>tabnew<cr>', opts)   -- Create a new tab.
-vim.api.nvim_set_keymap('n', '<leader>tx', '<cmd>tabclose<cr>', opts) -- Close a tab.
-vim.api.nvim_set_keymap('n', '<leader>tn', '<cmd>tabnext<cr>', opts)  -- Move to the previous tab.
-vim.api.nvim_set_keymap('n', '<leader>tp', '<cmd>tabprev<cr>', opts)  -- Move to the next tab.
-vim.api.nvim_set_keymap('n', '<leader>x', '<cmd>bd<cr>', opts)        -- Delete a buffer.
-vim.api.nvim_set_keymap('n', '<leader>p', '<cmd>bp<cr>', opts)        -- Move to the previous buffer.
-vim.api.nvim_set_keymap('n', '<leader>n', '<cmd>bn<cr>', opts)        -- Move to the next buffer.
+vim.api.nvim_set_keymap("v", "<C-c>", '"+y', opts)                                       -- Copy with Ctrl + C.
+vim.api.nvim_set_keymap('i', '<C-Backspace>', '<C-w>', opts)                             -- Ctrl + backspace.
+vim.api.nvim_set_keymap('i', '<C-h>', '<C-w>', opts)                                     -- Ctrl + backspace.
+vim.api.nvim_set_keymap('n', '<leader>tc', '<cmd>tabnew<cr>', opts)                      -- Create a new tab.
+vim.api.nvim_set_keymap('n', '<leader>tx', '<cmd>tabclose<cr>', opts)                    -- Close a tab.
+vim.api.nvim_set_keymap('n', '<leader>tn', '<cmd>tabnext<cr>', opts)                     -- Move to the previous tab.
+vim.api.nvim_set_keymap('n', '<leader>tp', '<cmd>tabprev<cr>', opts)                     -- Move to the next tab.
+vim.api.nvim_set_keymap('n', '<leader>x', '<cmd>bd<cr>', opts)                           -- Delete a buffer.
+vim.api.nvim_set_keymap('n', '<leader>p', '<cmd>bp<cr>', opts)                           -- Move to the previous buffer.
+vim.api.nvim_set_keymap('n', '<leader>n', '<cmd>bn<cr>', opts)                           -- Move to the next buffer.
 vim.api.nvim_set_keymap('n', '<leader>v', '<cmd>vsplit<cr><C-w>w<cmd>bp<cr><C-w>', opts) -- Split a buffer to the right.
 
 -- bufferline.
@@ -24,9 +26,9 @@ vim.api.nvim_set_keymap('n', '<leader>op', '<cmd>BufferLineMovePrev<cr>', opts) 
 vim.api.nvim_set_keymap('n', '<leader>on', '<cmd>BufferLineMoveNext<cr>', opts) -- Moves the selected buffer to the right of the buffer line.
 
 -- Telescope.
-vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', opts)   -- Fuzzy file search.
-vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', opts)    -- Fuzzy live grep.
-vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope buffers<cr>', opts)      -- Buffer list.
+vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', opts) -- Fuzzy file search.
+vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', opts)  -- Fuzzy live grep.
+vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope buffers<cr>', opts)    -- Buffer list.
 
 -- nvim-tree.
 vim.api.nvim_set_keymap('n', '<leader>e', '<cmd>NvimTreeToggle<cr>', opts)
