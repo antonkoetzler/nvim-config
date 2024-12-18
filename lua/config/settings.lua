072d6094608d463211365a0bc02db9301eaa3058
Anton Louis Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2024-12-18 03:38:33 -0300

    I HAVE NO PROBLEMS WITH MY NVIM NOW LETS GO

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -1,7 +1,8 @@
 -- Basic vim settings.
-vim.o.shiftwidth     = 2    -- 2 character tab length.
-vim.o.expandtab      = true -- Makes tabs spaces and not one character.
-vim.o.number         = true -- Numbers on the side of each file.
-vim.o.relativenumber = true -- Actual line number only on selected line, other lines are amount of lines from said line.
-vim.o.splitright     = true -- When :vsplit is used, it will focus on the newly created split.
-vim.o.smartindent    = true -- Indent based on current column number of cursor.
+vim.o.shiftwidth     = 2                                                           -- 2 character tab length.
+vim.o.expandtab      = true                                                        -- Makes tabs spaces and not one character.
+vim.o.number         = true                                                        -- Numbers on the side of each file.
+vim.o.relativenumber = true                                                        -- Actual line number only on selected line, other lines are amount of lines from said line.
+vim.o.splitright     = true                                                        -- When :vsplit is used, it will focus on the newly created split.
+vim.o.smartindent    = true                                                        -- Indent based on current column number of cursor.
+vim.api.nvim_create_autocmd('VimResized', { pattern = '*', command = 'wincmd =' }) -- Proportionally resize splits when terminal window is resized.
