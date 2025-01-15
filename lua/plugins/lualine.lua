3296368da2d9cf371a93e3b76f54035cae74549d
Anton Louis Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2025-01-15 15:19:21 -0300

    lualine full dir

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,6 +2,15 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    require('lualine').setup();
+    require('lualine').setup({
+      sections = {
+        lualine_c = {
+          {
+            'filename',
+            path = 2 -- 0 = just filename, 1 = relative path, 2 = absolute path
+          }
+        },
+      },
+    });
   end,
 }
