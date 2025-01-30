b729b8a57ae2b205201de569135661f5074bdf09
Anton Louis Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2025-01-30 15:11:06 -0300

    Forgive me

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -1,4 +1,4 @@
--- vim.cmd('colorscheme vim-moonfly-colors')
+vim.cmd('colorscheme vim-moonfly-colors')
 -- vim.cmd('colorscheme vim-nightfly-colors')
 -- vim.cmd('colorscheme bluloco')
-vim.cmd('colorscheme fluoromachine')
+-- vim.cmd('colorscheme fluoromachine')
