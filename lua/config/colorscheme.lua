d003f71c87dc405bcdc5c5decddba7e05bfdab6c
Anton Louis Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2025-01-30 14:42:18 -0300

    This is the way

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -1,1 +1,4 @@
-vim.cmd('colorscheme moonfly')
+-- vim.cmd('colorscheme vim-moonfly-colors')
+-- vim.cmd('colorscheme vim-nightfly-colors')
+-- vim.cmd('colorscheme bluloco')
+vim.cmd('colorscheme fluoromachine')
