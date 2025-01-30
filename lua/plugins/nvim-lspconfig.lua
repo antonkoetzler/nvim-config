123a7046a32c5f5051d87a35798944a26216154f
Anton Louis Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2025-01-30 07:54:21 -0300

    Automatic mason installed

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -7,63 +7,66 @@ return {
   config = function()
     local lspconfig = require('lspconfig')
     local on_attach = require('config.on_attach')
+    local mason_lspconfig = require('mason-lspconfig')
 
-    -- Lua.
-    lspconfig.lua_ls.setup({
-      on_attach = on_attach,
-      settings = {
-        Lua = {
-          diagnostics = {
-            globals = { 'vim' },
+    mason_lspconfig.setup({
+      automatic_installation = true,
+    })
+
+    local custom_settings = {
+      gopls = {
+        on_attach = function(client, bufnr)
+          on_attach(client, bufnr)
+          vim.bo[bufnr].expandtab = false
+          vim.bo[bufnr].tabstop = 4
+          vim.bo[bufnr].shiftwidth = 4
+        end,
+        settings = {
+          gopls = {
+            analyses = {
+              unusedparams = true,
+            },
+            staticcheck = true,
           },
         },
       },
-    })
+      tsserver = {
+        on_attach = on_attach,
+        settings = {
+          javascript = {
+            format = {
+              quoteStyle = "single",
+            },
+          },
+          typescript = {
+            format = {
+              quoteStyle = "single",
+            },
+          },
+        },
+      },
+      lua_ls = {
+        on_attach = on_attach,
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { 'vim' },
+            },
+          },
+        },
+      },
+    }
 
-    -- Go.
-    lspconfig.gopls.setup({
-      on_attach = function(cliente, bufnr)
-        on_attach(cliente, bufnr)
-        vim.bo[bufnr].expandtab = false
-        vim.bo[bufnr].tabstop = 4
-        vim.bo[bufnr].shiftwidth = 4
+    mason_lspconfig.setup_handlers({
+      function(server_name)
+        if custom_settings[server_name] then
+          lspconfig[server_name].setup(custom_settings[server_name])
+        else
+          lspconfig[server_name].setup({
+            on_attach = on_attach,
+          })
+        end
       end,
-      settings = {
-        gopls = {
-          analyses = {
-            unusedparams = true,
-          },
-          staticcheck = true,
-        },
-      },
     })
-
-    -- Typescript.
-    lspconfig.ts_ls.setup({
-      on_attach = on_attach,
-      settings = {
-        javascript = {
-          format = {
-            quoteStyle = "single"
-          }
-        },
-        typescript = {
-          format = {
-            quoteStyle = "single"
-          }
-        }
-      },
-    })
-
-
-    -- C/C++.
-    lspconfig.clangd.setup({
-      on_attach = on_attach,
-    });
-
-    -- PhP.
-    lspconfig.intelephense.setup({
-      on_attach = on_attach,
-    });
   end,
 }
