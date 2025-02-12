38d37134e257e25350ff9d7661ae8cb8606ebfee
Anton Louis Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2025-02-12 15:54:49 -0300

    Set background color to black always

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -4,3 +4,5 @@
 -- vim.cmd('colorscheme kanagawa')
 -- vim.cmd('colorscheme tokyonight');
 vim.cmd('colorscheme bamboo');
+
+vim.cmd("hi Normal guibg=#000000") -- Set the background color to black even with a colorscheme applied.
