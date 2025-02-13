ce4ebd6fa89864e7ee457fee3937c503d9b93d6e
Anton Louis Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2025-02-13 11:12:00 -0300

    moonfly never let me down

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -1,8 +1,6 @@
--- vim.cmd('colorscheme moonfly')
+vim.cmd('colorscheme moonfly')
 -- vim.cmd('colorscheme nightfly')
 -- vim.cmd('colorscheme seoul256')
 -- vim.cmd('colorscheme kanagawa')
 -- vim.cmd('colorscheme tokyonight');
-vim.cmd('colorscheme bamboo');
-
-vim.cmd("hi Normal guibg=#000000") -- Set the background color to black even with a colorscheme applied.
+-- vim.cmd('colorscheme bamboo');
