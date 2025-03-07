f89ea9685ad401780752c6080640962af4594a52
Anton Louis Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2025-03-07 08:52:35 -0300

    bamboo time

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -1,6 +1,6 @@
 -- vim.cmd('colorscheme moonfly')
 -- vim.cmd('colorscheme nightfly')
 -- vim.cmd('colorscheme seoul256')
-vim.cmd('colorscheme kanagawa')
+-- vim.cmd('colorscheme kanagawa')
 -- vim.cmd('colorscheme tokyonight');
--- vim.cmd('colorscheme bamboo');
+vim.cmd('colorscheme bamboo');
