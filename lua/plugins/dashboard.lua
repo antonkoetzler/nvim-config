9e3bfa5f0790ff99686dc1009b6052845b872197
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2025-03-19 16:22:42 -0300

    Hello neovim i cannot live life without you

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -8,7 +8,12 @@ return {
     local dashboard = require('alpha.themes.dashboard')
     local ascii = require('ascii')
     -- dashboard.section.header.val = dashboard.section.header.val
-    dashboard.section.header.val = ascii.art.animals.cats.luna
+    -- dashboard.section.header.val = ascii.art.animals.cats.luna
+    dashboard.section.header.val = {
+      "                                     ",
+      " _  _ _|_ _  _ |  _  _ _|_ _ | _   _ ",
+      "(_|| | |_(_)| ||<(_)(/_ |_ /_|(/_ | "
+    }
 
     require('alpha').setup(dashboard.opts)
   end,
