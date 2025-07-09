aecf4745a2f54d33526c21b264746b7d77dfc936
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2025-07-09 16:22:10 -0300

    qwoiejqwoiejwqioej

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -3,5 +3,18 @@ return {
   lazy = false,
   init = function()
     vim.g.vimtex_view_method = 'skim'
+    vim.g.vimtex_compiler_method = 'latexmk'
+    vim.g.vimtex_compiler_latexmk = {
+      executable = 'latexmk',
+      options = {
+        '-pdfxe',
+        '-file-line-error',
+        '-synctex=1',
+        '-interaction=nonstopmode',
+      },
+    }
+    vim.g.vimtex_compiler_latexmk_engines = {
+      _ = '-pdfxe'
+    }
   end
 }
