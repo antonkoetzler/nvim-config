5a858bef8fd7bf5eae3685c0412d9b56a9c820b0
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2025-08-27 23:24:47 -0300

    Chatgpt was useful this time

--- init.lua
+++ init.lua
@@ -1,2 +1,3 @@
-require('config.shortcuts') -- Shortcuts
-require('config.lazy') -- Bootstrap lazy.nvim and plugins
+require('config.settings')  -- Settings.
+require('config.shortcuts') -- Shortcuts.
+require('config.lazy')      -- Bootstrap lazy.nvim and plugins.
